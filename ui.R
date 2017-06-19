faf348c1b35ae4b0df448f9de1ed232cb47c8af7
sdhutchins <sdhutchins@users.noreply.github.com>
2017-06-19 10:34:43 -0500

    Add ui.R and server.R files

---
+++ ui.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(shinythemes)
+
+shinyUI(fluidPage(
+    fluidRow(HTML('<center><div class="jumbotron"><h1>Try Markdown</h1>
+                             <p>Input and edit Markdown formatted text and view output in real time.</p>
+                             </div></center>'),
+        column(6,
+               tags$textarea(
+                   'Hello markdown!',
+                   id    = 'markdown',
+                   rows  = 25,
+                   style = 'width:100%;'),
+               br(),
+               br(),
+               downloadButton('downloadData', 'Download Input')),
+ 
+        column(6,
+            htmlOutput('html'))),
+    hr(),
+    br(),
+    fluidRow(HTML('<center><div class="jumbotron"><h2>Markdown Basics</h2></div></center>'),
+             column(12,
+                    tags$iframe(style="height:800px; width:100%; scrolling=yes", 
+                                src="MarkdownBasics.pdf")))
+ 
+))
