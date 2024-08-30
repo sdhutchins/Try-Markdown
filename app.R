bf79905f9d76ccee0f2c0d87b1be3e74a533b277
sdhutchins <sdhutchins@uab.edu>
2024-08-29 19:43:22 -0500

    Fix app.R.

--- app.R
+++ app.R
@@ -6,53 +6,58 @@ md_message <- "## Hello markdown!
 Check out [my github](https://github.com/sdhutchins)."
 
 ui <- fluidPage(
-  fluidRow(
-    HTML('<center><div class="jumbotron"><h1>Try Markdown</h1>
-          <p>Input and edit Markdown formatted text and view output in real time.</p>
-          </div></center>'),
-    column(6,
-           tags$textarea(
-             value = md_message,
-             id    = 'markdown',
-             rows  = 25,
-             style = 'width:100%;'),
-           br(),
-           br(),
-           downloadButton('downloadData', 'Download Input')),
-    column(6,
-           uiOutput('html'))
-  ),
+  fluidRow(HTML('<center><div class="jumbotron"><h1>Try Markdown</h1>
+                             <p>Input and edit Markdown formatted text and view output in real time.</p>
+                             </div></center>'),
+           column(6,
+                  tags$textarea(
+                    md_message,
+                    id    = 'markdown',
+                    rows  = 25,
+                    style = 'width:100%;'),
+                  br(),
+                  br(),
+                  downloadButton('downloadData', 'Download Input')),
+           
+           column(6,
+                  htmlOutput('html'))),
   hr(),
   br(),
-  fluidRow(
-    HTML('<center><div class="jumbotron"><h2>Markdown Basics</h2></div></center>'),
-    column(12,
-           tags$iframe(style="height:800px; width:100%; scrolling=yes", 
-                       src="MarkdownBasics.pdf"))
-  )
+  fluidRow(HTML('<center><div class="jumbotron"><h2>Markdown Basics</h2></div></center>'),
+           column(12,
+                  tags$iframe(style="height:800px; width:100%; scrolling=yes", 
+                              src="MarkdownBasics.pdf")))
 )
 
 server <- function(input, output) {
   
-  output$html <- renderUI({
-    t <- tempfile(fileext = ".md")
+  output$html <- reactive({
+    
+    t <- tempfile()
     cat(input$markdown, file = t)
     
-    # Convert markdown to HTML
-    html_output <- markdown::markdownToHTML(t, fragment.only = TRUE)
+    ## convert input to html
+    t <- render(
+      input         = t,
+      output_format = 'html_document')
     
-    # Cleanup
-    unlink(t)
+    ## read results
+    res <- readLines(t)
     
-    # Return as HTML
-    HTML(html_output)
+    ## cleanup
+    unlink(sub('.html$', '*', t))
+    
+    ## return
+    paste(res, collapse = '\n')
+    
   })
   
   output$downloadData <- downloadHandler(
-    filename = function() { paste("markdowninput_", Sys.Date(), ".md", sep='') },
+    filename = function() { paste("markdowninput_", Sys.time(), ".md", sep='') },
     content = function(file) {
       write(input$markdown, file)
     })
 }
 
+# Wrap the ui and server in shinyApp
 shinyApp(ui = ui, server = server)
